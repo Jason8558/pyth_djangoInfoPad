8408ff7ce091c55face7f6e5c26864d2a8c22494
Jason8558 <bondarchyk9137@gmail.com>
2023-01-18 16:52:10 +1200

    Update settings.py

--- InfoPad/InfoPad/settings.py
+++ InfoPad/InfoPad/settings.py
@@ -77,10 +77,24 @@ WSGI_APPLICATION = 'InfoPad.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kvk_infopad',
+        'USER': 'hrd_user',
+        'PASSWORD': 'hrdpassword',
+        # 'HOST': '172.16.23.38',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
