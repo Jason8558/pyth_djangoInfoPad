4bfb54b3ad5260c0230250a10cd5e35515a1181d
Jason8558 <bondarchyk9137@gmail.com>
2023-01-18 17:03:55 +1200

    Update phonebook.js

--- InfoPad/phonebook/static/js/phonebook.js
+++ InfoPad/phonebook/static/js/phonebook.js
@@ -1,5 +1,5 @@
 function get_phones() {
-  url = 'http://172.16.23.48:8065/api-1/ldap-user-list'
+  url = 'http://172.16.23.38/ldap-user-list'
   $.getJSON(url,  (data) => {
     for (var i = 0; i < data.length; i++) {
       console.log(data[i].name);
