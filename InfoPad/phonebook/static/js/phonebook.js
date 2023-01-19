95954ee2ee8c7a50637e7ffe4b5d5660c343bfdf
Jason8558 <bondarchyk9137@gmail.com>
2023-01-19 14:12:39 +1200

    summary

--- InfoPad/phonebook/static/js/phonebook.js
+++ InfoPad/phonebook/static/js/phonebook.js
@@ -1,10 +1,21 @@
 function get_phones() {
-  url = 'http://172.16.23.38/ldapphones'
-  $.getJSON(url,  (data) => {
-    for (var i = 0; i < data.length; i++) {
-      console.log(data[1].name);
-    }
+  let phones
+  fetch("http://172.16.23.38/ldapphones/", {
+      method: "GET",
+      headers: {},
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        for (var el of res.list) {
+          console.log(el.cn);
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
 
 
-  })
+
 }
